cf018005a63a332aab5d3c2a4da1c8212801ec01
Emerson Pisoni <emersonpisoni14@gmail.com>
2021-04-26 03:37:56 -0300

    change config

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/jogo-da-forca" exact component={Home} />
         <Route path="/jogo" component={Jogo} />
         <Route path="/admin" component={Admin} />
         <Route path="/categorias" component={Categorias} />
