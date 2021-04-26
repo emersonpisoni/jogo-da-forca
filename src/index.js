37c64925be7d3186cfc45dc4db4cd5bdb02cca65
Emerson Pisoni <emersonpisoni14@gmail.com>
2021-04-26 03:33:26 -0300

    change package.json

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Jogo from "./pages/jogo";
 import Admin from "./pages/admin";
 import Categorias from "./pages/categorias";
+import { Home } from "./pages/home";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={App} />
+        <Route path="/" exact component={Home} />
         <Route path="/jogo" component={Jogo} />
         <Route path="/admin" component={Admin} />
         <Route path="/categorias" component={Categorias} />
