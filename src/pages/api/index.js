12916ba7b8bbe17fa1b4afbc558892192b9d3f58
Emerson Pisoni <emersonpisoni14@gmail.com>
2021-05-10 01:49:37 -0300

    change url

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://hangman-game-web-backend.herokuapp.com/api'
+  baseURL: 'https://hangman-game-web-backend.herokuapp.com/api'
 })
